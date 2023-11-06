5f9b47cd4480b045b44e09721d6bf402608f7f88
Xennia Contreras <Xenbydesign@gmail.com>
2023-11-05 18:15:39 -0800

    completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+//document.addEventListener('DOMContentLoaded',function(){
+//    console.log ('the Dom has loaded')
+//});
+//console.log ("This console.log() fires when index.js loads - before DOMContentLoaded is triggered");
+
+const text = document.getElementById('text')
+
+//console.log(text)
+
+function newText() { 
+    document.getElementById("text").textContent = "This is really cool!"
+}
+
+document.addEventListener('DOMContentLoaded', newText)
+
+//console.log(newText)
